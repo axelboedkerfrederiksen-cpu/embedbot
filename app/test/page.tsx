5f637ae38fe00d5d232436a12da216ca3921bceb
axelboedkerfrederiksen-cpu <axel.boedker.frederiksen@gmail.com>
2026-03-06 19:27:14 +0100

    add test page content

---
+++ app/test/page.tsx
@@ -0,0 +1,9 @@
+export default function TestPage() {
+  return (
+    <div>
+      <h1>Test hjemmeside</h1>
+      <p>Klik på chat boblen i hjørnet!</p>
+      <script src="/widget.js?id=dcec8d8c-1ab4-49c8-bf66-cd0f04f9e07f" />
+    </div>
+  );
+}
