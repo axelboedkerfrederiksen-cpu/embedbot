e78704a669ffea9eec3f44249dea3defdb40226b
axelboedkerfrederiksen-cpu <axel.boedker.frederiksen@gmail.com>
2026-03-06 19:47:19 +0100

    refactor: update TestPage structure to use HTML elements and localized content

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,8 +1,11 @@
-﻿export default function TestPage() {
+export default function TestPage() {
   return (
-    <div>
-      <h1>Test</h1>
-      <script src="/widget.js?id=dcec8d8c-1ab4-49c8-bf66-cd0f04f9e07f" />
-    </div>
+    <html>
+      <body>
+        <h1>Test hjemmeside</h1>
+        <p>Prøv at klikke på chat boblen i hjørnet!</p>
+        <script src="/widget.js?id=dcec8d8c-1ab4-49c8-bf66-cd0f04f9e07f" />
+      </body>
+    </html>
   );
 }
